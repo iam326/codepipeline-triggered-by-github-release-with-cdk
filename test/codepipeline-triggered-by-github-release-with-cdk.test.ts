039ec95528d7b2de9b49ace8cdf4f3e930509059
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2022-08-23 00:54:21 +0900

    Initial commit

---
+++ test/codepipeline-triggered-by-github-release-with-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodepipelineTriggeredByGithubReleaseWithCdk from '../lib/codepipeline-triggered-by-github-release-with-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/codepipeline-triggered-by-github-release-with-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodepipelineTriggeredByGithubReleaseWithCdk.CodepipelineTriggeredByGithubReleaseWithCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
